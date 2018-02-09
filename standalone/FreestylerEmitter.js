5a550c73b041ed587c72196440ec3bd351553566
Vadim Dalecky <streamich@gmail.com>
2018-02-09 02:17:54 +0000

    chore: add FreestylerEmitter to build

--- standalone/FreestylerEmitter.js
+++ standalone/FreestylerEmitter.js
@@ -1,8 +1,13 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ReduxEmitter = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.FreestylerEmitter = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 exports.__esModule = true;
-exports.default = ReduxEmitter;
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.default = FreestylerEmitter;
 
 var _sanitize = require('./helpers/sanitize');
 
@@ -14,26 +19,83 @@ var _createMessenger2 = _interopRequireDefault(_createMessenger);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function ReduxEmitter() {
-  var message = (0, _createMessenger2.default)('ReduxEmitter', 'Redux library');
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-  return function middleware(_ref) {
-    var getState = _ref.getState,
-        dispatch = _ref.dispatch;
+var sym = function sym(name) {
+  return '@@freestyler/' + name;
+};
+var $$instanceId = sym('debut/instanceId');
 
-    return function (next) {
-      return function (action) {
-        var result = next(action);
+var instanceId = 0;
 
-        message({
-          state: (0, _sanitize2.default)(getState()),
-          type: '@redux_ACTION',
-          action: (0, _sanitize2.default)(action)
-        });
-        return result;
-      };
-    };
-  };
+var getCompName = function getCompName(Comp, instance) {
+  var name = '<' + (Comp.displayName || Comp.name || 'Unknown') + '>';
+
+  if (instance) {
+    if (!instance[$$instanceId]) {
+      instance[$$instanceId] = instanceId++;
+    }
+
+    name += '[' + instance[$$instanceId].toString(36) + ']';
+  }
+
+  return name;
+};
+
+var reducer = function reducer(state, action) {
+  var Comp = action.Comp,
+      instance = action.instance;
+
+  var name = getCompName(Comp, instance);
+
+  switch (action.type) {
+    case 'RENDER':
+      {
+        var _extends2;
+
+        return _extends({}, state, (_extends2 = {}, _extends2[name] = action.styles, _extends2));
+      }
+    case 'UNRENDER':
+      {
+        // eslint-disable-next-line no-unused-vars
+        var omit = state[name],
+            rest = _objectWithoutProperties(state, [name]);
+
+        return rest;
+      }
+    case 'RENDER_STATIC':
+      {
+        var _extends3;
+
+        return _extends({}, state, (_extends3 = {}, _extends3[name] = action.styles, _extends3));
+      }
+  }
+
+  return state;
+};
+
+function FreestylerEmitter() {
+  var prefix = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+
+  var state = {};
+  var message = (0, _createMessenger2.default)('FreestylerEmitter', 'Freestyler library');
+  var channelName = sym('debug' + (prefix ? '/' + prefix : ''));
+  var broadcaster = window[channelName];
+
+  if ((typeof broadcaster === 'undefined' ? 'undefined' : _typeof(broadcaster)) !== 'object') {
+    var error = new Error('Freestyler debug channel not found at window[\'' + channelName + '\'].' + 'If you are using FREESTYLER_PREFIX env variable, you have to specify' + 'that in your kuker emitter, like FreestylerEmitter(myPrefix).');
+
+    console.error(error);
+  }
+
+  broadcaster.sub(function (action) {
+    state = reducer(state, action);
+
+    message({
+      state: (0, _sanitize2.default)(state),
+      type: '@freestyler_ACTION'
+    });
+  });
 };
 module.exports = exports['default'];
 },{"./helpers/createMessenger":2,"./helpers/sanitize":3}],2:[function(require,module,exports){
